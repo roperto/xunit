9163d965e3b39382b13f00313fbaf130bc0bb802
Daniel Thee Roperto <daniel@theeroperto.com>
2017-09-23 19:40:52 +1000

    Chapter 18

---
+++ meta-tdd.py
@@ -0,0 +1,28 @@
+class TestCase:
+    def __init__(self, name):
+        self.wasRun = None
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
+class TestCaseTest(TestCase):
+    def test_running(self):
+        test = WasRun("test_method")
+        assert not test.wasRun
+        test.run()
+        assert test.wasRun
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def test_method(self):
+        self.wasRun = 1
+
+
+TestCaseTest("test_running").run()
